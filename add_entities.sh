9f98d2a6402f778c7308f0a99db297233b85ddee
John Feras <jferas@ferasinfotech.com>
2022-12-06 13:01:10 -0500

    Added tenderly storage for contracts between block and transaction triggers.. not yet working

---
+++ add_entities.sh
@@ -0,0 +1,90 @@
+curl -X POST "https://api.tenderly.co/api/v1/account/me/project/project/addresses" --header "Content-Type: application/json" --header 'X-Access-Key: bC1rqQM2VD2h8edq1cK41OhXSOl-VxLs' --data-raw '{
+	"contracts": [
+        {
+            "network_id": "1",
+            "address": "0x004ce3e903af8b587f1dd2fe66800d0aef40843a",
+            "display_name": "0x004ce3e903af8b587f1dd2fe66800d0aef40843a"
+        },
+        {
+            "network_id": "1",
+            "address": "0x00676181c81fc033bd819fe41467c6cad5c23b1c",
+            "display_name": "0x00676181c81fc033bd819fe41467c6cad5c23b1c"
+        },
+        {
+            "network_id": "1",
+            "address": "0x00cc1f2313df5897f47d75c078946f1485a58d14",
+            "display_name": "0x00cc1f2313df5897f47d75c078946f1485a58d14"
+        },
+        {
+            "network_id": "1",
+            "address": "0x012a837008f3a36f585dc419ff6bceded6ca2f96",
+            "display_name": "0x012a837008f3a36f585dc419ff6bceded6ca2f96"
+        },
+        {
+            "network_id": "1",
+            "address": "0x01a528a9d11f02d99ddae44eaebeef4b367c58ea",
+            "display_name": "0x01a528a9d11f02d99ddae44eaebeef4b367c58ea"
+        },
+        {
+            "network_id": "1",
+            "address": "0x01b950d4b57f97bfdbf465a918b203cbd6ef169e",
+            "display_name": "0x01b950d4b57f97bfdbf465a918b203cbd6ef169e"
+        },
+        {
+            "network_id": "1",
+            "address": "0x024fdc692af50d28227a89279a33e1a443358817",
+            "display_name": "0x024fdc692af50d28227a89279a33e1a443358817"
+        },
+        {
+            "network_id": "1",
+            "address": "0x0293bbbc3c3ff0426b7a30baef6b6be7ff7e852e",
+            "display_name": "0x0293bbbc3c3ff0426b7a30baef6b6be7ff7e852e"
+        },
+        {
+            "network_id": "1",
+            "address": "0x02e18e8d98c6574138a8df45f1185627ee3aa321",
+            "display_name": "0x02e18e8d98c6574138a8df45f1185627ee3aa321"
+        },
+        {
+            "network_id": "1",
+            "address": "0x045b30b0e0e4e5fcab0ce6e2fbbfe8942f59df52",
+            "display_name": "0x045b30b0e0e4e5fcab0ce6e2fbbfe8942f59df52"
+        },
+        {
+            "network_id": "1",
+            "address": "0x047e3b250c3091021fd6e776bf60bbf6394e657c",
+            "display_name": "0x047e3b250c3091021fd6e776bf60bbf6394e657c"
+        },
+        {
+            "network_id": "1",
+            "address": "0x04ba3d4c372b295f9c1ded264d1d5700a55311c0",
+            "display_name": "0x04ba3d4c372b295f9c1ded264d1d5700a55311c0"
+        },
+        {
+            "network_id": "1",
+            "address": "0x0535b358f770703661ecf3cc3d18216e5f7bc648",
+            "display_name": "0x0535b358f770703661ecf3cc3d18216e5f7bc648"
+        },
+        {
+            "network_id": "1",
+            "address": "0x0547e5c6ff474f28765823a77271ab763234a5f2",
+            "display_name": "0x0547e5c6ff474f28765823a77271ab763234a5f2"
+        },
+        {
+            "network_id": "1",
+            "address": "0x05d2b8061e1f38e2fa6b05a21ebc9ed04f0cc94f",
+            "display_name": "0x05d2b8061e1f38e2fa6b05a21ebc9ed04f0cc94f"
+        },
+        {
+            "network_id": "1",
+            "address": "0x066221aa562c08aa774878c019b0c548cf8b3f11",
+            "display_name": "0x066221aa562c08aa774878c019b0c548cf8b3f11"
+        },
+        {
+            "network_id": "1",
+            "address": "0x082626c5bd7e32c33dbcbb17b166f47a8d846247",
+            "display_name": "0x082626c5bd7e32c33dbcbb17b166f47a8d846247"
+        }
+    ]
+}'
+
--- endaoment/example.ts
+++ endaoment/example.ts
@@ -12,53 +12,52 @@ const { ethers } = require("ethers");
 
 const { keccak256, toUtf8Bytes } = ethers.utils;
 
-// type Element = {
-// 	id: string;
-// 	contract: ContractElement;
-// }
+type ContractElement = {
+	id: string;
+	address: string;
+}
 
-const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".toLowerCase();
-const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7".toLowerCase();
-const someAddress = "0xC2F51C9DF556AF3742888aEd683981510aD57D5e".toLowerCase();
-const someOtherAddress = "0xCD531Ae9EFCCE479654c4926dec5F6209531Ca7b".toLowerCase();
+type Element = {
+	id: string;
+	contract: ContractElement;
+}
 
 const transferSig = keccak256(toUtf8Bytes("Transfer(address,address,uint256)"));
-
-let targetList = [usdc, usdt, someAddress, someOtherAddress];
 
 export const blockHelloWorldFn: ActionFn = async (context: Context, event: Event) => {
   let blockEvent = event as BlockEvent;
   console.log(`Got a block event.. its number is: ${blockEvent.blockNumber}`);
-//   try {
-//     const res = await axios.get('https://api.tenderly.co/api/v1/account/me/project/project/contracts?accountType=contract',
-// 	  {
-// 	    headers: {
-// 	    'X-Access-Key': 'bC1rqQM2VD2h8edq1cK41OhXSOl-VxLs',
-// 	  },
-//     });
-//     console.log(`We have ${res.data.length} configured contracts for monitoring`);
-// 	const elements: [] = res.data;
-//     elements.forEach(element => {
-// 		Object.keys(element).forEach(key => {
-// 		  console.log(`element key is: ${key}`);
-// 		  const contract = element.contract;
-// 		})
-// 		const contract = elemen
-// 		console.log(`contract address: ${element?.contract?.address}`);
-// 	});
-//   } catch (e) {
-//     console.log(`We got a POST error ${e}`)
-//   }
+	try {
+	  const res = await axios.get('https://api.tenderly.co/api/v1/account/me/project/project/contracts?accountType=contract',
+		{
+		  headers: { 'X-Access-Key': 'bC1rqQM2VD2h8edq1cK41OhXSOl-VxLs' },
+		});
+	  console.log(`We have ${res.data.length} configured contracts for monitoring`);
+	  const elements: Element[] = res.data;
+	  await context.storage.putJson("CONTRACTS", { elements: elements } );
+	} catch (e) {
+	  console.log(`We got a POST error ${e}`)
+	}
 }
 
 export const transactionExample: ActionFn = async (context: Context, event: Event) => {
-	let txEvent = event as TransactionEvent;
+	let targetList: string[] = [];
+	try {
+	  const elements: Element[] = await context.storage.getJson("CONTRACTS");
+	  elements.forEach(element => {
+		targetList.push(element.contract.address);
+	  });
+	} catch (e) {
+		console.log(`Error getting contract addresses from JSON storage - ${e}`);
+	}
+    let txEvent = event as TransactionEvent;
+	console.log(`targetList has ${targetList.length} contract addresses in it.`)
 	console.log(`Transaction has ${txEvent.logs.length} logs`);
 	let foundMatch = false;
     txEvent.logs.forEach(async (log) =>  {
 	  const topics = log.topics;
 	  if ( (topics.length == 3) && (topics[0] == transferSig) ) {
-		console.log(`Transaction has and ERC20 transfer in it: ${txEvent.hash}`);
+		console.log(`Transaction has an ERC20 transfer in it: ${txEvent.hash}`);
 		if (targetList.includes(topics[1].replace('0x000000000000000000000000', '0x'))) {
 		  foundMatch = true;
 		}
@@ -68,8 +67,8 @@ export const transactionExample: ActionFn = async (context: Context, event: Even
 	  }
 	});
 	if (foundMatch) {
-	try {
-		console.log(`ERC20 transfer in tx hash: ${txEvent.hash} ****************************************`);
+	  try {
+		console.log(`Entity ERC20 transfer in tx hash: ${txEvent.hash} ****************************************`);
 		await axios.post('https://b368-2603-7080-1b01-70e1-c14c-ec41-7716-beb8.ngrok.io//', { txhash: `${txEvent.hash}` }, {
 		headers: {
 			'content-type': 'text/json'
