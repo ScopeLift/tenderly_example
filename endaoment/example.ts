8c7f455c62661c1249a53294f5ba6f2769d69674
John Feras <jferas@ferasinfotech.com>
2022-12-08 17:46:50 -0500

    Got orgFundFactoryTrigger working... right now just console outputs the decoded created contract address from logs

--- endaoment/example.ts
+++ endaoment/example.ts
@@ -22,15 +22,43 @@ type Element = {
 	contract: ContractElement;
 }
 
-const transferSig = keccak256(toUtf8Bytes("Transfer(address,address,uint256)"));
+const orgFundFactoryAddress = "0x10fd9348136dcea154f752fe0b6db45fc298a589";
 
-export const blockHelloWorldFn: ActionFn = async (context: Context, event: Event) => {
+const transferSig = keccak256(toUtf8Bytes("Transfer(address,address,uint256)"));
+const entityDeployedSig = "0x1e3e29fb0c05e0c478577b9b3e207cbfe2952b1f9b239ed5d2535d4b24b6c577";
+
+export const blockTrigger: ActionFn = async (context: Context, event: Event) => {
   let blockEvent = event as BlockEvent;
   console.log(`Got a block event.. its number is: ${blockEvent.blockNumber}`);
   console.log(`transfer sig is: ${transferSig}`);
+  console.log(`entity deployed sig is: ${entityDeployedSig}`);
 }
 
-export const transactionExample: ActionFn = async (context: Context, event: Event) => {
+export const orgFundFactoryTrigger: ActionFn = async(context: Context, event: Event) => {
+  let txEvent = event as TransactionEvent;
+  if (txEvent.to == orgFundFactoryAddress) {
+    console.log(`we have an orgFundFactoryEvent with ${txEvent.logs.length}`);
+    let lognum = 0;
+    txEvent.logs.forEach(log =>  {
+      const topics = log.topics;
+      console.log(`  log ${lognum} has ${topics.length} topics`);
+      let topicnum = 0;
+      topics.forEach(topic => {
+        console.log(`    topicnum ${topicnum} is ${topic}`);
+        topicnum++;
+      });
+      lognum++;
+    });
+    const sig = txEvent.logs[1].topics[0];
+    if (sig == entityDeployedSig) {
+      const newEntityAddress = txEvent.logs[1].topics[1].replace('0x000000000000000000000000', '0x')
+      console.log(`we got an Entity address of ${newEntityAddress}`)
+    }
+  }  
+}
+
+export const erc20TransferTrigger: ActionFn = async (context: Context, event: Event) => {
+  let txEvent = event as TransactionEvent;
   let targetList: string[] = [];
   try {
     const res = await axios.get('https://api.tenderly.co/api/v1/account/me/project/project/contracts?accountType=contract',
@@ -45,13 +73,14 @@ export const transactionExample: ActionFn = async (context: Context, event: Even
   } catch (e) {
     console.log(`We got a POST error ${e}`)
   }
-  let txEvent = event as TransactionEvent;
   console.log(`targetList has ${targetList.length} contract addresses in it.`)
   console.log(`Transaction has ${txEvent.logs.length} logs`);
   let foundMatch = false;
   txEvent.logs.forEach(async (log) =>  {
   const topics = log.topics;
     if ( (topics.length == 3) && (topics[0] == transferSig) ) {
+      // include line below to send axios POST for every ERC20 transfer transaction hash (only target matches otherwise)
+      // foundMatch = true;
       console.log(`Transaction has an ERC20 transfer in it: ${txEvent.hash}`);
       if (targetList.includes(topics[1].replace('0x000000000000000000000000', '0x'))) {
         foundMatch = true;
@@ -63,7 +92,7 @@ export const transactionExample: ActionFn = async (context: Context, event: Even
   });
   if (foundMatch) {
     try {
-      console.log(`Entity ERC20 transfer in tx hash: ${txEvent.hash} ****************************************`);
+      console.log(`ERC20 transfer in tx hash: ${txEvent.hash} ****************************************`);
       await axios.post('https://b368-2603-7080-1b01-70e1-c14c-ec41-7716-beb8.ngrok.io//', { txhash: `${txEvent.hash}` }, {
         headers: {
           'content-type': 'text/json'
